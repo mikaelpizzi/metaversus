dd38ad7c79d6535b247da6162e2162de3b754d34
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-11-27 18:00:40 -0400

    feat: Add hero section text

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -11,8 +11,26 @@ const Hero = () => (
       initial='hidden'
       whileInView='show'
       viewport={{ once: false, amount: .25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
 
+      <div className='flex justify-center items-center flex-col relative z-10'>
+        <motion.h1
+          variants={textVariant(1.1)}
+          className={`${styles.heroHeading}`}
+        >
+          Metaverse
+        </motion.h1>
+
+        <motion.div
+          variants={textVariant(1.2)}
+          className='flex flex-row justify-center items-center'
+        >
+          <h1 className={`${styles.heroHeading}`}>Ma</h1>
+          <div className={`${styles.heroDText}`} />
+          <h1 className={`${styles.heroHeading}`}>Ness</h1>
+        </motion.div>
+      </div>
     </motion.div>
   </section>
 );
