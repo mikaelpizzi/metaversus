f6c7c8b2c89e7508fca04445acb044c4134acfa6
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-11-28 22:08:36 -0400

    feat: Add images to explore section

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -5,8 +5,13 @@ import { TypingText, ExploreCard, TitleText } from '../components';
 import styles from '../styles';
 import { staggerContainer } from '../utils/motion';
 import { exploreWorlds } from '../constants';
+import { useState } from 'react';
 
-const Explore = () => (
+const Explore = () => {
+  
+  const [active, setActive] = useState('world-2');
+
+  return (
   <section className={`${styles.paddings}`} id='explore'>
     <motion.div
       variants={staggerContainer}
@@ -23,8 +28,20 @@ const Explore = () => (
         title={<>Choose the world you want <br className='md:block hidden' />to explore</>}
         textStyles='text-center'
       />
+      <div className='mt-[50px] flex lg:flex-row flex-column min-h-[70vh] gap-5'>
+        { exploreWorlds.map((world, index) => (
+          <ExploreCard
+            key={world.id}
+            {...world}
+            index={index}
+            active={active}
+            handleClick={setActive}
+          />
+        ))}
+      </div>
     </motion.div>
   </section>
-);
+  )
+};
 
 export default Explore;
