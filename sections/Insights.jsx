1ce9584234a4ebc111a9c5721ff34c110d6cc676
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-11-30 11:38:07 -0400

    feat: Add 'Insight' section title

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,8 +1,30 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { InsightCard, TypingText, TitleText } from '../components';
+import styles from '../styles';
+import { staggerContainer } from '../utils/motion';
+import { insights } from '../constants';
+
 const Insights = () => (
-  <section>
-    Insights section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div 
+      variants={staggerContainer}
+      initial='hidden'
+      whileInView='show'
+      viewport={{ once: false, amount: 0.25}}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText
+        title='| Insight'
+        textStyles='text-center'
+      />
+      <TitleText
+        title='Insight about metaverse'
+        textStyles='text-center'
+      />
+
+    </motion.div>
   </section>
 );
 
