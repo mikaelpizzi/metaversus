25e33247b876847c4a70ba6e8de7a904fe23225d
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-11-29 21:36:27 -0400

    feat: Add 'World' section title

--- sections/World.jsx
+++ sections/World.jsx
@@ -1,8 +1,30 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { NewFeatures, TypingText, TitleText } from '../components';
+import styles from '../styles';
+import { staggerContainer, fadeIn } from '../utils/motion';
+
 const World = () => (
-  <section>
-    World section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div 
+      variants={staggerContainer}
+      initial='hidden'
+      whileInView='show'
+      viewport={{ once: false, amount: 0.25}}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText
+        title='| People on the World'
+        textStyles='text-center'
+      />
+      <TitleText
+        title={<>
+          Track friends around you and invite them to play together in the same world
+        </>}
+        textStyles='text-center'
+      />
+    </motion.div>
   </section>
 );
 
