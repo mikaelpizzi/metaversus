eddac0996152c2f5b3b62ad457ba1a0382004fc7
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-11-30 22:09:06 -0400

    feat: Add dividing line

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -33,6 +33,12 @@ const Footer = () => (
           >ENTER METAVERSE</span>
         </button>
       </div>
+
+      <div className="flex flex-col">
+        <div className="mb-[50px] h-[2px] bg-white opacity-10" />
+      </div>
+
+
     </div>
   </motion.footer>
 );
