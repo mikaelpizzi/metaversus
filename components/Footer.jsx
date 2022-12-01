552616ba7fe498ad1c0a5fedb690bf3fd7cc241d
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-11-30 22:20:07 -0400

    feat: Add footer content and styles

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -36,6 +36,27 @@ const Footer = () => (
 
       <div className="flex flex-col">
         <div className="mb-[50px] h-[2px] bg-white opacity-10" />
+
+        <div className="flex items-center justify-between flex-wrap gap-4">
+          <h4
+            className="font-extrabold text-[24px] text-white"
+          >Metaversus</h4>
+          <p
+            className="font-normal text-[14px] text-white opacity-50"
+          >Copyright © 2022 Mikael Pizzi. All rights reserved.</p>
+          <div
+            className="flex gap-4"
+          >
+            { socials.map(social => (
+              <img
+                key={social.name}
+                src={social.url}
+                alt={social.name}
+                className='w-[24px] h-[24px] object-contain cursor-pointer'
+              />
+            ))}
+          </div>
+        </div>
       </div>
 
 
